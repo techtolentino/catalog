354ef3107c6af8e75016c4c5bf0f31cf63464315
Randy Tolentino <makertolentino@gmail.com>
2016-12-26 12:49:31 -0600

    first commit :rocket:

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var browserSync = require('browser-sync');
+var prefix 		= require('gulp-autoprefixer');
+var cp          = require('child_process');
+
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+/** * Build the Jekyll Site */
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+/** Rebuild Jekyll & do page reload when watched files change */
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+gulp.task('sass', function () {
+    return gulp.src('_scss/main.scss')
+        .pipe(sass({
+            includePaths: ['scss'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 2 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('_site/src/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('src/css'));
+});
+
+/** Wait for jekyll-build, then launch the Server */
+gulp.task('serve', ['sass', 'jekyll-build'], function() {
+    browserSync.init({
+        server: "_site/"
+    });
+});
+
+/** Watch all files for changes, except the _site and other unneccessary folders */
+gulp.task('watch', function () {
+    gulp.watch('_scss/*.*', ['sass']);
+    gulp.watch(['**/*.*', '!_site/**/*', '!node_modules/**/*','!.sass-cache/**/*' ], ['jekyll-rebuild']);
+});
+
+/**Default task, running just `gulp` will compile the jekyll site, launch BrowserSync & watch files. */
+gulp.task('default', ['serve', 'watch']);
---
+++ index.md
@@ -0,0 +1,17 @@
+---
+layout: default
+title: Slack Catalog
+description: Search for internal, customized integrations or bots that can be integrated with Slack.
+---
+<div class="band band--MEDIUM">
+    <div class="container container--MEDIUM">
+        <div class="align--CENTER text--WHITE">
+            <h1>{{page.title}}</h1>
+            <p>{{page.description}}</p>
+            <div class="form--search">
+                <input class="form--input" type="text" placeholder="Search for integration">
+                <button class="form--submit">🔎</button>
+            </div>
+        </div>
+    </div>
+</div>
